832abaf9577c7bc3cdd6de706781f558bce0eabe
Swapnendu Saha <Swapnendusaha376@gmail.com>
2026-02-10 20:38:13 +0530

    Initial backend folder structure

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
