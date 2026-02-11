f6f066f224a6f676d763b488963c9c45b433689b
Swapnendu Saha <Swapnendusaha376@gmail.com>
2026-02-11 18:33:50 +0530

    Category schema and model created

---
+++ backend/models/Categories/Category.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    shares: {
+      type: Number,
+      default: 0,
+    },
+    posts: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Category = mongoose.model("Category",categorySchema);
+module.exports = Category;
