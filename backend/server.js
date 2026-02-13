5df62a5f9129b2ca6546d34f794acad33b310838
Swapnendu Saha <Swapnendusaha376@gmail.com>
2026-02-13 19:03:47 +0530

    Connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8080;
+const connectDB = require("./config/databaseConnection");
+connectDB();
+
 
 app.get("/", (req, res) => {
   res.send("Home page");
