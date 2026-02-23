c7580de37d00a669132a35eec398b7daaf233279
Swapnendu Saha <Swapnendusaha376@gmail.com>
2026-02-23 23:18:27 +0530

    createCategory controller added

--- backend/controllers/categories/categoriesController.js
+++ backend/controllers/categories/categoriesController.js
@@ -1,1 +1,22 @@
 const Category = require("../../models/Categories/Category");
+
+//@desc create new category
+//@route POST /api/V1/categories
+//@access private
+
+module.exports.createCategory = async (req, res, next) => {
+  const { name } = req.body;
+  const category = await Category.findOne({ name });
+  if (category) {
+    throw new Error("Category already exists");
+  } else {
+    const newCategory = new Category({ name, author: req?.userAuth?._id });
+    await newCategory.save();
+    res.json({
+      status: "Success",
+      message: "Category created successfully",
+      newCategory,
+    });
+  }
+};
+
