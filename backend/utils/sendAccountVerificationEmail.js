f85289893191c1de2ef196ae3d1b3d2265649191
Swapnendu Saha <Swapnendusaha376@gmail.com>
2026-03-18 21:01:27 +0530

    Removed dotenv.config() statement

--- backend/utils/sendAccountVerificationEmail.js
+++ backend/utils/sendAccountVerificationEmail.js
@@ -1,7 +1,4 @@
 const nodemailer = require("nodemailer");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const sendAccountVerificationEmail = async (email, verificationToken) => {
   try {
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,7 +1,4 @@
 const nodemailer = require("nodemailer");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const sendEmail = async (email, resetToken) => {
   try {
